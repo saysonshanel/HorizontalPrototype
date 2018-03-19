616789080f1ea2f6d44c9ad0f0bd6381b889c4b8
saysonshanel <ssayson@ucalgary.ca>
2018-03-18 20:32:03 -0700

    edit profile complete

--- HorizontalPrototype/MainWindow.xaml.cs
+++ HorizontalPrototype/MainWindow.xaml.cs
@@ -25,55 +25,79 @@ namespace HorizontalPrototype
         {
             InitializeComponent();
 
+            //When the login button is pressed on the main screen, direct to login screen
             Storyboard lginsb = this.FindResource("SplashDisappear") as Storyboard;
-            lginsb.Completed += OnSplashDisappearComplete;
+            lginsb.Completed += LoginButtonComplete;
 
+
+            //when the signup button is clicked, direct to registration screen
             Storyboard sb1 = this.FindResource("SplashDisappear1") as Storyboard;
-            sb1.Completed += Sb1_Completed;
+            sb1.Completed += SignupButtonComplete;
 
+            //hide other canvas's
             this.LoginScreenCanvas.Visibility = Visibility.Hidden;
             this.SignupScreenCanvas.Visibility = Visibility.Hidden;
             this.EditScreenViewer.Visibility = Visibility.Hidden;
+
+            //when facebook or google connection is clicked,  move to login
+            this.FacebookButton.Click += LoginButtonComplete;
+            this.GoogleButton.Click += LoginButtonComplete;
+
         }
 
-        private void Sb1_Completed(object sender, EventArgs e)
+        //sign up screen
+        private void SignupButtonComplete(object sender, EventArgs e)
         {
+            //hide other screens
             this.LoginScreenCanvas.Visibility = Visibility.Hidden;
             this.MainScreenCanvas.Visibility = Visibility.Hidden;
+            //show signup screen
             this.SignupScreenCanvas.Visibility = Visibility.Visible;
 
-            this.SignupButton1.Click += SaveButton_Click;
-
+            //when signup button is clicked, direct to edit profile
+            this.RegisterSignupButton.Click += ToEditProfileScreen;
+            //otherwise back button is clicked, direct to main screen
             this.BackButton1.Click += OnBackButtonClicked;
 
         }
 
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.SignupScreenCanvas.Visibility = Visibility.Hidden;
-            this.EditScreenViewer.Visibility = Visibility.Visible;
-        }
-
+        //move back to main screen
         private void OnBackButtonClicked(object sender, RoutedEventArgs e)
         {
+            //hide the other screens
             this.LoginScreenCanvas.Visibility = Visibility.Hidden;
             this.SignupScreenCanvas.Visibility = Visibility.Hidden;
+
+            //show the main screen
             Storyboard sb = this.FindResource("SplashAppear") as Storyboard;
             this.MainScreenCanvas.Visibility = Visibility.Visible;
             sb.Begin();
 
         }
 
-        private void OnSplashDisappearComplete(object sender, EventArgs e)
+        //loginscreen
+        private void LoginButtonComplete(object sender, EventArgs e)
         {
+            //hide the main screen
             this.MainScreenCanvas.Visibility = Visibility.Hidden;
+            //show the login screen
             this.LoginScreenCanvas.Visibility = Visibility.Visible;
 
+            //when login button is clicked, move to the edit profile screen
+            this.LoginScreenLoginButton.Click += ToEditProfileScreen;
+
+            //otherwise, if back button is clicked, go back to main screen
             this.BackButton.Click += OnBackButtonClicked;
 
         }
 
-
+        //edit profile screen
+        private void ToEditProfileScreen(object sender, RoutedEventArgs e)
+        {
+            this.LoginScreenCanvas.Visibility = Visibility.Hidden;
+            this.SignupScreenCanvas.Visibility = Visibility.Hidden;
+            this.EditScreenViewer.Visibility = Visibility.Visible;
+        }
     }
 
 }
