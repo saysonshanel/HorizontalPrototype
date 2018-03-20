d9c4642ce40cd4a39f24eb883e8ef29f4fe4ff40
GilMobson <35181080+GilMobson@users.noreply.github.com>
2018-03-19 19:29:26 -0600

    Add files via upload

--- HorizontalPrototype/MatchInit.xaml.cs
+++ HorizontalPrototype/MatchInit.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace USeek
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MatchInit.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MatchInit : Window
     {
-        public MainWindow()
+        public MatchInit()
         {
             InitializeComponent();
-
         }
     }
 }
--- HorizontalPrototype/SwipeUp.xaml.cs
+++ HorizontalPrototype/SwipeUp.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace USeek
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SwipeUp.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SwipeUp : Window
     {
-        public MainWindow()
+        public SwipeUp()
         {
             InitializeComponent();
-
         }
     }
 }
